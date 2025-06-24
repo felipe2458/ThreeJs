86fc1951f69ad987c591bbc2ebeec46874c0d3f3
felipe2458 <luizfelipealvesdematos10@gmail.com>
2025-06-24 10:18:01 -0300

    adicionando sombras

--- src/app/pages/home/children/initial/initial.component.ts
+++ src/app/pages/home/children/initial/initial.component.ts
@@ -19,6 +19,10 @@ export class InitialComponent {
 
       //* Criando o renderizador
       const renderer = new THREE.WebGLRenderer();
+
+      //* Definindo as sombras
+      renderer.shadowMap.enabled = true;
+
       renderer.setSize(width, height);
 
       //* Adicionando o renderizador ao container
@@ -44,13 +48,15 @@ export class InitialComponent {
 
       //* Criando e adicionando a caixa ao cenário
       const boxGeometry = new THREE.BoxGeometry();
-      const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x10aab3 });
+      const boxMaterial = new THREE.MeshLambertMaterial({ color: 0x10aab3 });
       const box = new THREE.Mesh(boxGeometry, boxMaterial);
       scene.add(box);
+      //box.position.set(0, 2, 0);
 
       //* Criando e adicionando o plano ao cenário
       const planeGeometry = new THREE.PlaneGeometry(30, 30);
-      const planeMaterial = new THREE.MeshBasicMaterial({
+      //* Para o plano receber a sombra de outros elementos, é necessário mudar o material dele(não funciona com MeshBasicMaterial)
+      const planeMaterial = new THREE.MeshStandardMaterial({
         color: 0x10676b,
         //* criando o "outro" lado do plano. Sem isso, quando você olha atrás do plano, não aparece a cor
         side: THREE.DoubleSide,
@@ -58,24 +64,64 @@ export class InitialComponent {
       const plane = new THREE.Mesh(planeGeometry, planeMaterial);
       scene.add(plane)
       plane.rotation.x = -Math.PI / 2;
+      //* Habilitando a sombra (recebida)
+      plane.receiveShadow = true;
 
       //* Criando e adicionando uma grade ao cenário
       const gridHelper = new THREE.GridHelper(30); //* Pode-se passar um segundo argumento para definir a quantidade de divisões da grade
       scene.add(gridHelper);
 
       //* Criando e adicionando uma esfera ao cenário
-
       //* Pode-se passar 3 argumentos, o primeiro define o tamanho da esfera, o segundo define a quantidade de subdivisões horizontal(em outras palavras, a segunda definie o tanto de lados que a "esfera" vai ter na horizontal) e o terceiro define a quantidade de subdivisões vertical(em outras palavras, a terceira definie o tanto de lados que a "esfera" vai ter na vertival)
       const sphereGeometry = new THREE.SphereGeometry(3, 30, 40);
-      const sphereMaterial = new THREE.MeshBasicMaterial({
+      //* Para a esfera ser alterada pela luz de ambiente, é necessário mudar o material dela(não funciona com MeshBasicMaterial)
+      const sphereMaterial = new THREE.MeshStandardMaterial({
         color: 0x00ff62,
         //* Mostra a grade do elemento ao invés de pinta-la por completo (true)
         wireframe: false
        });
       const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
       scene.add(sphere);
-
       sphere.position.set(-10, 10, 0);
+      //* Habilitando sombra (recebida e enviada)
+      sphere.castShadow = true;
+
+      /*
+      //* Adicionando uma luz de ambiente
+      const ambientLight = new THREE.AmbientLight(0x333333);
+      scene.add(ambientLight);
+      */
+
+      /*
+      //* Adicionando uma luz direcional
+      const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.6);
+      scene.add(directionalLight);
+      //* Posição da luz direcional
+      directionalLight.position.set(-30, 50, 0);
+      //* Habilitando sombra (enviada)
+      directionalLight.castShadow = true;
+      //* Alterando o tamanho da sombra
+      directionalLight.shadow.camera.bottom = -12;
+
+      //* Adicionando um ajudante pra a luz direcional
+      const dLigthHelper = new THREE.DirectionalLightHelper(directionalLight);
+      scene.add(dLigthHelper);
+
+      //* Adicionando ajudando para a sombra
+      const dlightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+      scene.add(dlightShadowHelper);
+      */
+
+      //* Adicionando uma luz holofote
+      const spotLight = new THREE.SpotLight(0xFFFFFF);
+      scene.add(spotLight);
+      spotLight.position.set(-100, 350, 0);
+      spotLight.castShadow = true;
+      spotLight.angle = 0.05;
+
+      //* Adicionando ajudante pra a luz holofote
+      const sLightHelper = new THREE.SpotLightHelper(spotLight);
+      scene.add(sLightHelper);
 
       //* Adicionando a biblioteca de controle de animação
       const gui = new dat.GUI()
@@ -84,12 +130,16 @@ export class InitialComponent {
       const options = {
         sphereColor: "#00ff62",
         wireframe: false,
-        speed: 0.01
+        speed: 0.01,
+        angle: 0.2,
+        penumbra: 0,
+        intensity: 10000,
+        axle: {  x: -100, y: 100, z: 0 },
       }
 
-      gui.addColor(options, 'sphereColor').onChange(function(e){
+      /*gui.addColor(options, 'sphereColor').onChange(function(e){
         sphere.material.color.set(e)
-      })
+      })*/
 
       gui.add(options, 'wireframe').onChange(function(e){
         sphere.material.wireframe = e
@@ -98,6 +148,14 @@ export class InitialComponent {
       //* Fazendo a esfera "saltar"
       gui.add(options, 'speed', 0, 0.1)
 
+      //* Opções de sombra
+      gui.add(options, 'angle', 0, 1)
+      gui.add(options, 'penumbra', 0, 1)
+      gui.add(options, 'intensity', 0, 1000000)
+      gui.add(options.axle, 'x', -200, 200)
+      gui.add(options.axle, 'y', -200, 200)
+      gui.add(options.axle, 'z', -200, 200)
+
       let step = 0;
 
       //* Animação para a caixa ficar girando
@@ -105,8 +163,20 @@ export class InitialComponent {
         box.rotation.x += 0.01;
         box.rotation.y += 0.01;
 
+        //* Fazendo a esfera saltar
         step += options.speed
+        //* O primeiro valor é o quanto a esfera vai subir
         sphere.position.y = 10 * Math.abs(Math.sin(step))
+
+        //* Opções de sombra
+        spotLight.angle = options.angle
+        spotLight.penumbra = options.penumbra
+        spotLight.intensity = options.intensity
+        sLightHelper.update()
+
+        //* Opções de eixo do holofote
+        spotLight.position.set(options.axle.x, options.axle.y, options.axle.z)
+        sLightHelper.update()
 
         renderer.render(scene, camera);
       }
